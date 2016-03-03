7c245ea116e49c627d9c07f29d702e1253ec1ca3
Nithin Bekal <nithinbekal@gmail.com>
2016-03-03 19:16:19 +0530

    Use Todo.Cache to allow multiple lists

---
+++ lib/todo/cache.ex
@@ -0,0 +1,28 @@
+defmodule Todo.Cache do
+  use GenServer
+
+  def start do
+    GenServer.start(__MODULE__, nil)
+  end
+
+  def server_process(cache_pid, todo_list_name) do
+    GenServer.call(cache_pid, {:server_process, todo_list_name})
+  end
+
+  def init(_) do
+    {:ok, HashDict.new}
+  end
+
+  def handle_call({:server_process, todo_list_name}, _, todo_servers) do
+    case HashDict.fetch(todo_servers, todo_list_name) do
+      {:ok, todo_server} ->
+        {:reply, todo_server, todo_servers}
+
+      :error ->
+        {:ok, new_server} = Todo.Server.start
+        servers = HashDict.put(todo_servers, todo_list_name, new_server)
+
+        {:reply, new_server, servers}
+    end
+  end
+end
