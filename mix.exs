30c1809a7018c01ebfd19605dfd5060ec46c9750
Nithin Bekal <nithinbekal@gmail.com>
2016-03-09 23:46:59 +0530

    Make the project an application

--- mix.exs
+++ mix.exs
@@ -14,7 +14,10 @@ defmodule Todo.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [
+      applications: [:logger],
+      mod: {Todo.Application, []}
+    ]
   end
 
   # Dependencies can be Hex packages:
